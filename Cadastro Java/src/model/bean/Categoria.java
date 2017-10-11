a6418f89767d2d9224b183cec6b5b4674373bcba
Leandro Nunes <leandro_nunes1@estudante.sc.senai.br>
2017-10-10 22:00:51 -0300

    Interface visual Swing

--- Cadastro Java/src/model/bean/Categoria.java
+++ Cadastro Java/src/model/bean/Categoria.java
@@ -41,5 +41,11 @@ public class Categoria {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    @Override
+    public String toString() {
+        return getDescricao(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
